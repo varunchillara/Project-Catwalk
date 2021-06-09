39f9d66baf3f56c213722d69e916f4f8b95a132d
Suliman Tekalli <sulimantekalli@gmail.com>
2021-06-09 15:44:42 -0400

    add average rating function and make related products ratings dynamic

--- client/related_products/RelatedProducts.jsx
+++ client/related_products/RelatedProducts.jsx
@@ -19,6 +19,25 @@ class cardTemplate extends React.Component {
     }
 
     this.populateCard = this.populateCard.bind(this);
+    this.getAverageRating = this.getAverageRating.bind(this);
+  }
+
+  getAverageRating = (reviews) => {
+    let getTotalReviews = (reviews) => {
+      let total = 0;
+      for (let key in reviews) {
+        total += Number(reviews[key]);
+      }
+      return total;
+    }
+      let getTotalRatings = (reviews) => {
+        let total = 0;
+        for (let key in reviews) {
+          total += Number(key) * Number(reviews[key]);
+        }
+      return total;
+    }
+    return getTotalReviews(reviews) / getTotalRatings(reviews);
   }
 
   populateCard () {
@@ -29,74 +48,53 @@ class cardTemplate extends React.Component {
           axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${relatedProduct}`))
         let relatedProductsThumbnails = dummyRelatedProductIds.map(relatedProduct =>
           axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${relatedProduct}/styles`))
+        let relatedProductsReviews = dummyRelatedProductIds.map(relatedProduct =>
+          axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/meta`, {params: {
+            product_id: relatedProduct
+          }}))
         // let relatedProductsReviews = dummyRelatedProductIds.map(relatedProduct =>
         //   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${relatedProduct}/reviews`))
-        return Promise.all(relatedProductsData.concat(relatedProductsThumbnails))
+        return Promise.all(relatedProductsData.concat(relatedProductsThumbnails).concat(relatedProductsReviews))
         })
       .then(results => {
 
 
-       /*
 
-        {
-          11007: {
-            image: STYLES,
-            category: PRODUCT_INFO,
-            nameWithText: PRODUCT_INFO,
-            defaultPrice: STYLES,
-            salesPrice STYLES,
-            rating: RATING
-          },
-          11008: {
-              etc.
-          }...
-       */
 
-      let allRelatedProducts = {}
-      for (let i = 0; i < results.length; i++) {
-        let data = {};
-        //IF STYLE
-        if (results[i].data.id === undefined) {
-          //it is a style data
-          let defaultStyleIndex = 0;
-          //find default style
-          results[i].data.results.some((style, index)=> {
-            if (style['default?']) {
-              defaultStyleIndex = index;
-              return true;
-            } else {
-              return false;
-            }
-          })
-          //add price, sales price, thumbnail
-          let id = results[i].data.product_id
+        let allRelatedProducts = {}
+        for (let i = 0; i < results.length; i++) {
+          let data = results[i].data;
+          let id = data.id || Number(data.product_id);
           //SET THE ID IF IT DOESN'T YET EXIST
           if (allRelatedProducts[id] === undefined) {
-            allRelatedProducts[id] = {}
-            allRelatedProducts[id].id = Number(id);
-          }
-          allRelatedProducts[id].original_price = results[i].data.results[defaultStyleIndex].original_price
-          allRelatedProducts[id].sale_price = results[i].data.results[defaultStyleIndex].sale_price
-          allRelatedProducts[id].photo = results[i].data.results[defaultStyleIndex].photos[0].url
-          //TAKE CARE OF REVIEWS EVENTUALLY
-          allRelatedProducts[id].rating = 3;
-        } else {
-            //IF PRODUCT_INFO
-            let id = results[i].data.id
-            if (allRelatedProducts[id.toString()] === undefined) {
-              allRelatedProducts[id.toString()] = {}
-              allRelatedProducts[id.toString()].id = id;
+            allRelatedProducts[id] = {
+              id: id
             }
-            allRelatedProducts[id].category = results[i].data.category
-            allRelatedProducts[id].nameWithText = results[i].data.name;
-            //TAKE CARE OF REVIEWS EVENTUALLY
-            allRelatedProducts[id].rating = 3;
+          }
+          //IF IS RATING
+          if (data.ratings) {
+            allRelatedProducts[id].rating = this.getAverageRating(results[i].data.ratings)
+          //ELSE IF STYLES
+          } else if (data.product_id) {
+            let defaultStyleIndex = 0;
+            let defaultFound = false
+            data.results.some((style, index)=> {
+              style['default?'] ? (defaultStyleIndex = index, defaultFound = true) : defaultFound;
+              return defaultFound;
+            })
+            allRelatedProducts[id].original_price = data.results[defaultStyleIndex].original_price
+            allRelatedProducts[id].sale_price = data.results[defaultStyleIndex].sale_price
+            allRelatedProducts[id].photo = data.results[defaultStyleIndex].photos[0].url
+            //ELSE IF PRODUCT_INFO
+          } else {
+            allRelatedProducts[id].category = data.category
+            allRelatedProducts[id].nameWithText = data.name;
           }
         }
         this.setState ({
           dummyRelatedProductsData: Object.values(allRelatedProducts)
         })
-        console.log(this.state.dummyRelatedProductsData)
+        console.log('current State', this.state.dummyRelatedProductsData)
       })
       .catch(error => {
         console.error(error)
@@ -118,20 +116,23 @@ class cardTemplate extends React.Component {
 }
 
 const Card = (props) => {
+  let inlineStyle = {
+    width: '150px'
+  }
   return (
     <div>
       <hr></hr>
       <div><strong>An image</strong></div>
-      {JSON.stringify(props.data.photo)}
+      <img src={props.data.photo} alt="NO THUMBNAIL" style={inlineStyle}></img>
       <div><strong>Category</strong></div>
-      {JSON.stringify(props.data.category)}
+      {props.data.category}
       <div><strong>Expanded Product Name with extra text</strong></div>
-      {JSON.stringify(props.data.nameWithText)}
+      {props.data.nameWithText}
       <div><strong>Price</strong></div>
-      {JSON.stringify(props.data.orignal_price)}
-      {JSON.stringify(props.data.sale_price)}
+      {props.data.original_price}
+      {props.data.sale_price}
       <div><strong>Rating</strong></div>
-      {JSON.stringify(props.data.rating)}
+      {props.data.rating}
       <hr></hr>
     </div>
   )
