83f50b77f732fb1b386e0881ed1780e5b4701572
Suliman Tekalli <sulimantekalli@gmail.com>
2021-06-15 17:34:12 -0400

    Implememt cacheing system

--- client/App.jsx
+++ client/App.jsx
@@ -22,7 +22,6 @@ function App () {
     currentStyle: useSelector(state => state.currentStyle),
     currentMetaReviews: useSelector(state => state.currentMetaReviews)
   }
-
   useEffect(() => {
     console.log(cachedData)
     if (cachedData['11004'] === undefined || cachedData[`${dataToBeCached.currentProduct.data.id}`] === undefined) {
@@ -36,7 +35,6 @@ function App () {
         cachedData[dataToBeCached.currentProduct.data.id] = dataToBeCached;
         dispatch(updateCache(cachedData));
       }
-      console.log('updating from cached')
       dispatch(updateAll(cachedData[currentAppId]));
     } else {
       axios.defaults.headers = {
