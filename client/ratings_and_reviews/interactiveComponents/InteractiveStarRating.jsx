5ffe84db587e3422b386d6f95355e851100cdf8c
varunchillara <varun@Varuns-MacBook-Pro-2.local>
2021-06-18 09:16:03 -0400

    interactive stars bug fixed

--- client/ratings_and_reviews/interactiveComponents/InteractiveStarRating.jsx
+++ client/ratings_and_reviews/interactiveComponents/InteractiveStarRating.jsx
@@ -3,13 +3,13 @@ import React, {useState} from 'react';
 const InteractiveStarRating = (props) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-
+//
   return (
     <div className="star-rating">
       {[...Array(5)].map((star, index) => {
         index += 1;
         return (
-          <button
+          <span
             id="starButton"
             type="button"
             key={index}
@@ -21,8 +21,8 @@ const InteractiveStarRating = (props) => {
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(rating)}
           >
-            <span className="star2">&#9733;</span>
-          </button>
+            <span className="fa fa-star star"></span>
+          </span>
         );
       })}
     </div>
